56a4dd132f29aa365a575b5a81a13d3a0d48ee9a
Antonio Sucre <antonio.sucre@indikatore.com>
2021-09-30 19:48:25 -0300

    add-form-implementation

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './conversor-moedas.css';
 import { Jumbotron, Button, Form, Col, Row, Spinner, Alert, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import ListarMoedas from './listar-moedas';
 
+
+
 function ConversorMoedas() {
+
+  const [valor, setValor] = useState('1');
+  const [moedaDe, setMoedaDe] = useState('BRL');
+  const [moedaPara, setMoedaPara] = useState('USD');
+  const [exibirSpinner, setExibirSpinner] = useState(false);
+  const [formValidado, setFormValidado] = useState(false);
+
+  function handleValor(event) {
+    setValor(event.target.value.replace(/\D/g, ''));
+  }
+
+  function handleMoedaDe(event) {
+    setMoedaDe(event.target.value)
+  }
+
+  function handleMoedaPara(event) {
+    setMoedaPara(event.target.value)
+  }
+
+  function converter(event) {
+    event.preventDefault();
+    setFormValidado(true);
+    if (event.currentTarget.checkValidity === true) {
+      //TODO implementar a chamada ao Fixer.io
+    }
+
+  }
+
   return (
     <div>
-
       <h1>Conversor de Moedas</h1>
-      <Alert variant="danger" show={false}>Erro obtendo dados de conversão, tente novamente</Alert>
+      <Alert variant="danger" show={false}>
+        Erro obtendo dados de conversão, tente novamente
+      </Alert>
+
       <Jumbotron>
-        <Form>
+        <Form onSubmit={converter} noValidate validated={formValidado}>
           <Row>
             <Col sm="3">
-              <Form.Control placeholder="0" value={1} required />
+              <Form.Control
+                placeholder="0"
+                value={valor}
+                onChange={handleValor}
+                required />
             </Col>
 
             <Col sm="3">
-              <Form.Control as="select">
+              <Form.Control as="select"
+                value={moedaDe}
+                onChange={handleMoedaDe}>
                 <ListarMoedas />
               </Form.Control>
             </Col>
@@ -29,15 +67,21 @@ function ConversorMoedas() {
             </Col>
 
             <Col sm="3">
-              <Form.Control as="select">
+              <Form.Control as="select"
+                value={moedaPara}
+                onChange={handleMoedaPara}>
                 <ListarMoedas />
               </Form.Control>
             </Col>
 
             <Col sm="2">
               <Button variant="success" type="submit">
-                <Spinner animation="border" size="sm" />
-                Converter
+                <span className={exibirSpinner ? null : 'hidden'}>
+                  <Spinner animation="border" size="sm" />
+                </span>
+                <span className={exibirSpinner ? 'hidden' : null}>
+                  Converter
+                </span>
               </Button>
             </Col>
           </Row>

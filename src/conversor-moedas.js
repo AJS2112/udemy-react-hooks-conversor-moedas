faaa52bb61f9663cc5f3363709e55b7446aeee89
Antonio Sucre <antonio.sucre@indikatore.com>
2021-09-30 05:28:20 -0300

    making-html-form

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import './conversor-moedas.css';
+import { Jumbotron, Button, Form, Col, Row, Spinner } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 function ConversorMoedas() {
   return (
-    <h1>Conversor de Moedas</h1>
+    <div>
+
+      <h1>Conversor de Moedas</h1>
+      <Jumbotron>
+        <Form>
+          <Row>
+            <Col sm="3">
+              <Form.Control placeholder="0" value={1} required />
+            </Col>
+
+            <Col sm="3">
+              <Form.Control as="select">
+
+              </Form.Control>
+            </Col>
+
+            <Col sm="1" className="text-center" style={{ paddingTop: '5px' }}>
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </Col>
+
+            <Col sm="3">
+              <Form.Control as="select">
+
+              </Form.Control>
+            </Col>
+
+            <Col sm="2">
+              <Button variant="success" type="submit">
+                <Spinner animation="border" size="sm" />
+                Converter
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Jumbotron>
+    </div>
   );
 }
 

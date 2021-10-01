8751db8994e9c35465d4fcc226ad2f8cac7d9199
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-01 04:39:42 -0300

    add-modal-implementation

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -14,6 +14,8 @@ function ConversorMoedas() {
   const [moedaPara, setMoedaPara] = useState('USD');
   const [exibirSpinner, setExibirSpinner] = useState(false);
   const [formValidado, setFormValidado] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+  const [resultadoConversao, setResultadoConversao] = useState('');
 
   function handleValor(event) {
     setValor(event.target.value.replace(/\D/g, ''));
@@ -27,11 +29,20 @@ function ConversorMoedas() {
     setMoedaPara(event.target.value)
   }
 
+  function handleFecharModal(event) {
+    setValor('1');
+    setMoedaDe('BRL');
+    setMoedaPara('USD');
+    setFormValidado(false);
+    setExibirModal(false);
+  }
+
   function converter(event) {
     event.preventDefault();
     setFormValidado(true);
-    if (event.currentTarget.checkValidity === true) {
+    if (event.currentTarget.checkValidity() === true) {
       //TODO implementar a chamada ao Fixer.io
+      setExibirModal(true);
     }
 
   }
@@ -87,15 +98,15 @@ function ConversorMoedas() {
           </Row>
         </Form>
 
-        <Modal show={false}>
+        <Modal show={exibirModal} onHide={handleFecharModal}>
           <Modal.Header closeButton>
             <Modal.Title>Conversão</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Resultado da conversão aqui...
+            {resultadoConversao}
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="success">Nova conversão</Button>
+            <Button variant="success" onClick={handleFecharModal}>Nova conversão</Button>
           </Modal.Footer>
         </Modal>
       </Jumbotron>

d919c588077d5b62d5e4ee454dc17f0f11dfa3ff
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-01 05:47:16 -0300

    add-unit-tests

--- src/conversor-moedas.test.js
+++ src/conversor-moedas.test.js
@@ -1,6 +1,26 @@
-import { render } from '@testing-library/react';
 import ConversorMoedas from './conversor-moedas';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import axiosMock from 'axios';
 
-test('deve renderizar o componente sem erros', () => {
-  render(<ConversorMoedas />);
-});
+describe('Teste de componente de conversao de moeadas', () => {
+
+  test('deve renderizar o componente sem erros', () => {
+    render(<ConversorMoedas />);
+  });
+
+  it('deve simular uma conversao de moeadas', async () => {
+    const { findByTestId, getByTestId } = render(<ConversorMoedas />);
+    axiosMock.get.mockResolvedValueOnce({
+      data: { success: true, rates: { BRL: 4.564292, USD: 1.101049 } }
+    });
+
+    fireEvent.click(getByTestId('btn-converter'));
+    const modal = await findByTestId('modal');
+
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(modal).toHaveTextContent('1 BRL = 0.24 USD');
+  });
+
+})
+
